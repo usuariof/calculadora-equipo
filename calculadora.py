1c7b955026011da580b2aa3772b59239e7f1c1d7
usuariof <fidelpatricio111@gmail.com>
2026-05-20 15:10:41 -0300

    actualizacion de suma y resta calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,16 +1,10 @@
-num1=float(input("ingrese el primer numero:"))
-num2=float(input("Ingrese el segundo numero:"))
-print ("operaciones")
-print ("1: Suma")
-print ("2: Resta")
-print ("3: multiplicacion")
-print ("4: division")
-operacion = input("Elige una operacion:")
-if (operacion == "1"):
-    resultado = num1+num2
-    print(resultado)
+num1 = float(input("Ingrese el primer número: "))
+operacion = input("Ingrese la operación (+, -, x, /): ")
+num2 = float(input("Ingrese el segundo número: "))
+if operacion=="+":
+    print("Resultado:", num1+num2)
 else:
- if (operacion == "2"):
-    resultado = num1-num2
-    print(resultado)
+    if operacion=="-":
+        print("Resultado:", num1-num2)
+    else
 
